29658fc3325ed701b9d23b4b65de9d9d6d39f5e9
rakesh <raakhys@gmail.com>
2021-11-19 13:40:21 +0530

    rakesh-2 add to mvntest2 git repository

---
+++ rakesh-2/src/main/java/com/example2/rakesh2/Rakesh2Application.java
@@ -0,0 +1,13 @@
+package com.example2.rakesh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rakesh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rakesh2Application.class, args);
+	}
+
+}
